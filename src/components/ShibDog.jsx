b6693a9d9b41708f57c279a0abd47d8d918541b0
fedengarcia <43423670+fedengarcia@users.noreply.github.com>
2024-04-20 15:14:29 +0200

    Create ShibDog.jsx

---
+++ src/components/ShibDog.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyledShibDog } from "./styledComponents/Containers";
+
+export default function ShibDog() {
+  return <StyledShibDog>
+    <div class="bubble-dog">I can get you a thumb by three o'clock</div>
+    <img className="shib-dog" src="./dog-inu.png"/>
+  </StyledShibDog>;
+}
